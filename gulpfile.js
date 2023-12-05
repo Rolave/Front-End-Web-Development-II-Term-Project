ad9357c16c443b95ab0da77ee8b8ef219af4741a
Rodrigo Olave Carrasco <rolave@Rodrigos-MacBook-Pro-2.local>
2023-12-05 15:28:02 -0500

    feat: create landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-function buildStyles() {
+const buildStyles = function () {
   return src('sass/**/*.scss')
     .pipe(sass())
-    .pipe(dest('dist/assets/css'));
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(dest('public/assets/css'));
 }
 
-function watchTask() {
+const watchTask = function () {
   watch(['sass/**/*.scss'], buildStyles);
 }
 
